315c89f40d72210f048fef6ec3db1f41b4dc3184
mhmd75mmm0-sudo <mhmd75mmm0@gmail.com>
2026-06-25 20:35:19 +0300

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,16 +1,50 @@
+import { handleCustomerUpdate } from "./handlers/customer.js";
+import { handleAdminUpdate } from "./handlers/admin.js";
+
 export default {
   async fetch(request, env, ctx) {
-    return new Response(
-      JSON.stringify({
-        status: "success",
-        project: "MHMD Activation Bot",
-        message: "Worker is running successfully!"
-      }, null, 2),
-      {
-        headers: {
-          "Content-Type": "application/json"
-        }
+
+    if (request.method !== "POST") {
+      return new Response("MHMD Activation Bot", {
+        status: 200
+      });
+    }
+
+    let update;
+
+    try {
+      update = await request.json();
+    } catch {
+      return new Response("Invalid JSON", {
+        status: 400
+      });
+    }
+
+    const token = request.headers.get("x-telegram-bot-api-secret-token");
+
+    try {
+
+      if (token === env.BOT_TOKEN) {
+        await handleCustomerUpdate(update, env);
       }
-    );
+
+      else if (token === env.ADMIN_BOT_TOKEN) {
+        await handleAdminUpdate(update, env);
+      }
+
+      else {
+        return new Response("Unauthorized", {
+          status: 401
+        });
+      }
+
+    } catch (err) {
+
+      console.log(err);
+
+    }
+
+    return new Response("OK");
+
   }
-};
+}
